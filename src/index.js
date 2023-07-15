d9d229b52af9b25ffd68dabcdb59a76d01c0485b
chezee4 <vadimdufinec@gmail.com>
2023-07-15 15:52:41 +0300

    added MarvelService

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import App from './components/app/App';
+
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+const  marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(data => data.data.results.forEach(item => console.log(item.name)))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
 
