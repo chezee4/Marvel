bad3cc61960f7d650154b211fbbd08727f3ef4f5
chezee4 <vadimdufinec@gmail.com>
2023-07-16 16:59:10 +0300

    added spinner, errorMessage

---
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+import img from './error.gif';
+import "./errorMessage.scss";
+const ErrorMessage = () => {
+    return (
+        <img className='error-message'  src={img} alt="Error"/>
+    )
+}
+
+export default ErrorMessage;
