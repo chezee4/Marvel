ce3641033afe44b42f5fc3db090281ba6a82e030
chezee4 <vadimdufinec@gmail.com>
2023-07-23 18:51:29 +0300

    fix bug with LazyLoading

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,7 +2,7 @@ import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 
-const LazyLoading  = lazy(() => import('../lazyLoading/LazyLoading'));
+import LazyLoading from '../lazyLoading/LazyLoading';
 const Page404 = lazy(() => import('../pages/404'));
 const HeroesPage = lazy(() => import('../pages/HeroesPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
@@ -14,17 +14,17 @@ const App = () =>{
         return (
             <Router basename="/Marvel">
                 <div className="app">
-                <Suspense fallback={<LazyLoading/>}>
                     <AppHeader/>
-                    <main style={{height:'80vh'}}>
-                        <Routes>
-                            <Route path="/" element={<HeroesPage/>}/>
-                            <Route path="/comics" element={<ComicsPage/>}/>
-                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                            <Route path="*" element={<Page404/>}/>
-                        </Routes>
+                    <main style={{"min-height":'80vh'}}>
+                       <Suspense fallback={<LazyLoading/>}>
+                            <Routes>
+                                <Route path="/" element={<HeroesPage/>}/>
+                                <Route path="/comics" element={<ComicsPage/>}/>
+                                <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                                <Route path="*" element={<Page404/>}/>
+                            </Routes>
+                        </Suspense>
                     </main>
-                    </Suspense>
                 </div>
             </Router>
         )
